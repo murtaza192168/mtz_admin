6ebbe4457605fd897e06db5ec567ebe3b8427ae7
murtaza192168 <murtazacloudwork1252@gmail.com>
2024-10-16 17:05:20 +0530

    Initial commit - backend setup

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Basic route to check server is running
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
